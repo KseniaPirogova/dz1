b09b7f653d471614b157fc247f69038c3b0f730e
Ksenia <senia.91@mail.ru>
2023-01-18 20:21:00 +0700

    создала программу на максимальное число из 3 чисел

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿//Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+
+Console.WriteLine("введите число 1");
+int x = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("введите число 2");
+int y = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("введите число 3");
+int z = Convert.ToInt32(Console.ReadLine());
+
+if (x>y && x>z) Console.WriteLine(x); 
+
+if (y>x && y>z) Console.WriteLine(y); 
+
+if (z>y && z>x) Console.WriteLine(z); 
+
+
+
+
