2c17aea4156f9d4cc1f9aa636efb2ffac724fdd5
Ksenia <senia.91@mail.ru>
2023-01-18 19:34:26 +0700

    создали задачу на большее число

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("введите число 1");
+int x = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("введите число 2");
+int y = Convert.ToInt32(Console.ReadLine());
+
+if (x>y)
+{
+    Console.WriteLine("максимально первое число"); 
+}
+else 
+{
+   Console.WriteLine("максимальное второе число");  
+}
